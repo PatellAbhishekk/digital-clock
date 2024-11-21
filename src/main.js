0087f0aec7995b11f6c1e91429dd931d27a71508
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-11-21 09:53:46 +0000

    update js file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-const
+const $ = (selector) => {
+  return document.querySelector(selector);
+};
+
+const hour = $(".hour");
+const dot = $(".dot");
+const minute = $(".minute");
+const week = $(".week");
+let showDot = true;
+
+function update() {
+  showDot = !showDot;
+  const now = new Date();
+
+  if (showDot) {
+    dot.classList.add("invisible");
+  } else {
+    dot.classList.remove("invisible");
+  }
+
+  // Use String() instead of string(), and padStart() instead of padstart()
+  hour.textContent = String(now.getHours()).padStart(2, "0");
+  minute.textContent = String(now.getMinutes()).padStart(2, "0");
+
+  Array.from(week.children).forEach((ele) => {
+    ele.classList.remove("active");
+  });
+
+  // Fix the index for getDay() since it returns a number from 0 to 6
+  week.children[now.getDay()].classList.add("active");
+}
+
+setInterval(update, 500);
